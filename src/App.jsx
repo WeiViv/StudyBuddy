70c83cfb760d9acf28c7d508b1d28ce660059cd4
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-06 13:55:14 -0500

    style(theme): Update MUI theme to NU Purple.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import FirstTimeUser from './components/FirstTimeUser';
@@ -8,6 +9,7 @@ import GroupsPage from './components/GroupsPage';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import ProfilePage from './components/ProfilePage';
+
 import './App.css';
 
 const AppContent = ({ currentPage, setCurrentPage }) => {
@@ -16,20 +18,35 @@ const AppContent = ({ currentPage, setCurrentPage }) => {
   // Check if the current page is the first-time user page
   const isFirstTimeUserPage = location.pathname === '/first-time-user';
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#4E2A84',
+      },
+      secondary: {
+        main: '#f44336',
+      },
+    },
+  });
+
   return (
     <>
-      {/* Conditionally render based on the current route */}
-      {!isFirstTimeUserPage && <Header />}
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="groups" element={<GroupsPage />} />
-          <Route path="messages" element={<div>TBD</div>} />
-          <Route path="profile/:id" element={<ProfilePage />} />
-          <Route path="first-time-user" element={<FirstTimeUser />} />
-        </Routes>
-      </div>
-      {!isFirstTimeUserPage && <Footer currentPage={currentPage} setCurrentPage={setCurrentPage} />}
+      <ThemeProvider theme={theme}>
+        {/* Conditionally render based on the current route */}
+        {!isFirstTimeUserPage && <Header />}
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="groups" element={<GroupsPage />} />
+            <Route path="messages" element={<div>TBD</div>} />
+            <Route path="profile/:id" element={<ProfilePage />} />
+            <Route path="first-time-user" element={<FirstTimeUser />} />
+          </Routes>
+        </div>
+        {!isFirstTimeUserPage && (
+          <Footer currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        )}
+      </ThemeProvider>
     </>
   );
 };
